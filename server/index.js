d4a69da6ba353c906b1569b16df54c32cd622a23
Václav Oborník <register.ufon@gmail.com>
2018-12-26 00:32:54 +0100

    added material design components

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ let koaStatic = require('koa-static');
 
 app.use(koaStatic(path.join(__dirname, '../client')));
 
-const server = app.listen(process.env.PORT || 3033);
+const port = process.env.PORT || 3033;
+
+const server = app.listen(port, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Server is up and listening on port ${port}`);
+});
 
 module.exports = server;
